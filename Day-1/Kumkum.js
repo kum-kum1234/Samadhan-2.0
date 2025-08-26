5d500320eb1006c300897fbefab4867289e426d0
Kumkum Goyal <145197845+kum-kum1234@users.noreply.github.com>
2025-08-26 23:41:54 +0530

    Kumkum.js

---
+++ Day-1/Kumkum.js
@@ -0,0 +1,11 @@
+// Single student object
+const student = {
+  name: "Kumkum",
+  rollNo: 101,
+  course: "CSE"
+};
+
+// Printing details
+console.log(`Name: ${student.name}`);
+console.log(`Roll No: ${student.rollNo}`);
+console.log(`Course: ${student.course}`);
