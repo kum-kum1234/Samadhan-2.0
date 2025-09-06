f78b9c8b6aba25bac7bb97e0b6ae7d09e3420a51
Kumkum Goyal <145197845+kum-kum1234@users.noreply.github.com>
2025-09-06 22:09:46 +0530

    App.js

---
+++ Day-15/frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
