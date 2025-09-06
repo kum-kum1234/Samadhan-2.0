e98058a6ac31fcd023a2cc004e8c8e423371c6b3
Kumkum Goyal <145197845+kum-kum1234@users.noreply.github.com>
2025-09-06 22:16:16 +0530

    CheckoutForm.js

---
+++ Advance_project_day-16/frontend/src/components/CheckoutForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [message, setMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) return;
+
+    setIsLoading(true);
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: `${window.location.origin}`,
+      },
+    });
+
+    if (error.type === "card_error" || error.type === "validation_error") {
+      setMessage(error.message);
+    } else {
+      setMessage("An unexpected error occurred.");
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button disabled={isLoading || !stripe || !elements} id="submit">
+        <span id="button-text">
+          {isLoading ? "Processing..." : "Pay now"}
+        </span>
+      </button>
+      {message && <div>{message}</div>}
+    </form>
+  );
+};
+
+export default CheckoutForm;
