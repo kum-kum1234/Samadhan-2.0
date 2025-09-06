e859d2cded8b727868d2433b3d0a9587667a1ba9
Kumkum Goyal <145197845+kum-kum1234@users.noreply.github.com>
2025-09-06 22:30:23 +0530

    Create User.js

---
+++ Advance_project_day-16/backend/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cart: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+      },
+      quantity: {
+        type: Number,
+        default: 1
+      }
+    }
+  ]
+});
+
+module.exports = mongoose.model('User', UserSchema);
