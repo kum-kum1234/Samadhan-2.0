71fdff2209464a5f538a05ad6ef4ced3b7443868
Kumkum Goyal <145197845+kum-kum1234@users.noreply.github.com>
2025-09-06 22:30:02 +0530

    Create Product.js

---
+++ Advance_project_day-16/backend/models/Product.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true }, // Store price in cents
+  imageUrl: { type: String, required: true },
+});
+module.exports = mongoose.model('Product', productSchema);
